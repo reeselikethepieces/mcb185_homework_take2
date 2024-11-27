a43e5f94c600bbc5bfee285fb6ea9c23d1d995a5
reeselikethepieces <charisse.carlson@gmail.com>
2024-11-26 18:03:41 -0800

    new

--- 13spellingbee.sh
+++
@@ -1,3 +0,0 @@
-gunzip -c ~/Code/MCB185/data/dictionary.gz | grep  -v "[^zirconia]" | grep "r" | grep -E ".{4,}"
-
-# would like to discuss; was struggling with and looked back at previous code, understood, but have questions why iterations I tried did not work
