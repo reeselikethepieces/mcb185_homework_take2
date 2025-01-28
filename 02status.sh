2e35c2323e130d89cb1cb3519f00ca7e303a776c
reeselikethepieces <charisse.carlson@gmail.com>
2025-01-28 15:10:55 -0800

    up

---
+++ 02status.sh
@@ -0,0 +1,11 @@
+date
+uname -a
+printenv
+python3 --version
+ls -R ~/Code
+
+# reports date
+# reports full name of unix flavor
+# reports environment variables
+# " " 	  version of python installed
+# " " 	  directories and files in Code directory 
