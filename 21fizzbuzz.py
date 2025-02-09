e8cffa1b3c8ca8d0ee12093026c3b3f42ea10aaa
reeselikethepieces <charisse.carlson@gmail.com>
2025-02-09 09:56:39 -0800

    buzzbuzz

--- 21fizzbuzz.py
+++ 21fizzbuzz.py
@@ -12,9 +12,8 @@ while True:
 	if i == 100: break
 
 
-i = 1
-for i in range(100):
-	i += 1
+
+for i in range(1, 100):
 	if i % 3 == 0: print(i, 'Fizz')
 	if i % 5 == 0: print(i, 'Buzz')
 	if i % 3 == 0 and i % 5 == 0: print(i,'FizzBuzz')
