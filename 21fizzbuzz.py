fe7b49d0ab2f677f4980b98ebc9f85832c0a1c2f
reeselikethepieces <charisse.carlson@gmail.com>
2025-02-11 09:11:24 -0800

    up

--- 21fizzbuzz.py
+++ 21fizzbuzz.py
@@ -1,19 +1,21 @@
 # writes out #s from 1 to 100
-# if # is divisible by 3, write Fizz
-# if # is divisible by 5, write Buzz
+# if # is divisible by 3, write Fizz, instead
+# if # is divisible by 5, write Buzz, instead
 # if both, write FizzBuzz
 
 i = 0
 while True:
 	i += 1
-	if i % 3 == 0: print(i, 'Fizz')
-	if i % 5 == 0: print(i, 'Buzz')
-	if i % 3 == 0 and i % 5 == 0: print(i,'FizzBuzz')
+	if i % 3 == 0 and i % 5 == 0: print('FizzBuzz')
+	elif i % 3 == 0: print('Fizz')
+	elif i % 5 == 0: print('Buzz')
+	else: print(i)
 	if i == 100: break
 
 
 
-for i in range(1, 100):
-	if i % 3 == 0: print(i, 'Fizz')
-	if i % 5 == 0: print(i, 'Buzz')
-	if i % 3 == 0 and i % 5 == 0: print(i,'FizzBuzz')
+for i in range(1, 101):
+	if i % 3 == 0 and i % 5 == 0: print('FizzBuzz')
+	elif i % 3 == 0: print('Fizz')
+	elif i % 5 == 0: print('Buzz')
+	else: print(i)
